02d2312e7416644890babc09411e99e18b123df9
Yu Rui Ji <128909624+Aristo23333@users.noreply.github.com>
2024-03-14 23:08:55 +0800

    Add files via upload

--- models/__init__.py
+++ models/__init__.py
@@ -1,9 +1,3 @@
-# --------------------------------------------------------
-# OctFormer: Octree-based Transformers for 3D Point Clouds
-# Copyright (c) 2023 Peng-Shuai Wang <wangps@hotmail.com>
-# Licensed under The MIT License [see LICENSE for details]
-# Written by Peng-Shuai Wang
-# --------------------------------------------------------
 
 from .PointMamba import PointMamba
 from .PointMambaseg import PointMambaSeg
